330693e3f503e15fe6551772bd1139266dab5e15
Ms-Alex <ms.hernandeza1@gmail.com>
2018-10-03 12:37:05 -0400

    DRY up info card

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import UUID from 'uuid';
 
 class InfoCard extends Component {
 
@@ -7,22 +8,31 @@ class InfoCard extends Component {
         return "Not Set or Invisible."
     }
 
+    userInfo = () => {
+        let infoView = []
+        for (let dataKey in this.props.user) {
+            infoView.push([dataKey, this.checkInfo(this.props.user[dataKey])])
+        }
+        return infoView;
+    }
+
+    upCaseElement = (element) => {
+        return element.charAt(0).toUpperCase() + element.slice(1)
+    }
+
+    userInfoMapper = () => {
+        return this.userInfo().map(info => <div key={UUID()}>
+            <h4>{this.upCaseElement(info[0]).split('_').join(' ')}</h4>
+            {info[1]}
+        </div>)
+    }
+
     render() {
         return (
             <div>
                 <h2>Searched For: {this.props.user.login}</h2>
 
-                <h4>Name: </h4> 
-                {this.checkInfo(this.props.user.name)}
-
-                <h4>Email: </h4>
-                {this.checkInfo(this.props.user.email)}
-
-                <h4>Company: </h4>
-                {this.checkInfo(this.props.user.company)}
-
-                <h4>Number of Public Repos: </h4>
-                {this.checkInfo(this.props.user.public_repos)}
+                {this.userInfoMapper()}
 
                 <a href={this.props.user.html_url} target="_blank">
                     <h3>- Visit {this.props.user.login}'s Login Page -</h3>
